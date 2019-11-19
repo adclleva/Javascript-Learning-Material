a9aa728f1f3b5f04b81cf3e792b2d2207cad1852
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-18 21:08:39 -0800

    added example solutions for the Digit Raised One

--- Javascript-Code-Wars/Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!.js
+++ Javascript-Code-Wars/Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!.js
@@ -42,3 +42,25 @@ let x = 21
 console.log(x.toString().split(""))
 console.log(sumDigitRaised(1))
 console.log(sumDigPow(1,100))
+
+// function sumDigPow(a, b) {
+//     var ans = [];
+//     while(a <= b){
+//       if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+//         ans.push(a);
+//       a++;
+//     }
+//     return ans;
+//   }
+
+//   function sumDigPow(a, b) {
+//     var arr = [];
+//     for (var i = a; i <= b; i++) {
+//       var sum = 0;
+//       for (var j = 0; j <= String(i).length; j++) {
+//         sum += Math.pow(parseInt(String(i)[j]), j+1);  
+//         if (sum == i) arr.push(i);
+//       }
+//     }
+//     return arr;
+//   }
